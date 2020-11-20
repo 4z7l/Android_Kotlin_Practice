de4a2627f420be76bdd8501061ff663f0a28119d
4z7l <7d4z7l@naver.com>
2020-11-20 09:34:21 +0900

    chore : kakao login

--- app/src/main/java/com/igluesmik/android_kotlin_practice/LoginActivity.kt
+++ app/src/main/java/com/igluesmik/android_kotlin_practice/LoginActivity.kt
@@ -3,41 +3,42 @@ package com.igluesmik.android_kotlin_practice
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.util.Utility
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // 로그인 공통 callback 구성
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Log.e(TAG, "로그인 실패", error)
-            }
-            else if (token != null) {
-                Log.i(TAG, "로그인 성공 ${token.accessToken}")
+        //로그인 공통 콜백 구성
+        val callback: ((OAuthToken?, Throwable?) -> Unit) = { token, error ->
+            if (error != null) { //Login Fail
+                Log.e(TAG, "Kakao Login Failed :", error)
+            } else if (token != null) { //Login Success
                 startMainActivity()
             }
         }
-
-        kakao_login_btn.setOnClickListener{
-            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
-                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
-            } else {
-                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+        findViewById<ImageView>(R.id.kakao_login_btn).setOnClickListener {
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니라면 카카오계정으로 로그인
+            LoginClient.instance.run {
+                if (isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                    loginWithKakaoTalk(this@LoginActivity, callback = callback)
+                } else {
+                    loginWithKakaoAccount(this@LoginActivity, callback = callback)
+                }
             }
         }
     }
 
-    fun startMainActivity() {
+    private fun startMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    companion object {
+        private const val TAG = "LoginActivity"
     }
 }
--- app/src/main/java/com/igluesmik/android_kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/igluesmik/android_kotlin_practice/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.igluesmik.android_kotlin_practice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.kakao.sdk.auth.LoginClient
-import com.kakao.sdk.auth.model.OAuthToken
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
 }
